07dde3aff7712dfd713b5a41c56393b31ad7b33e
Marcelo <marceloo.dev@gmail.com>
2025-03-12 15:38:39 -0300

    Add JWT authentication to user login and registration

--- routes/public.js
+++ routes/public.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
-
 const router = express.Router();
+const JWT_SECRET = process.env.JWT_SECRET;
 
+//cadastro
 router.post("/cadastro", async (req, res) => {
   try {
     const user = req.body;
@@ -22,7 +24,37 @@ router.post("/cadastro", async (req, res) => {
     });
     res.status(201).json(userDB);
   } catch (error) {
-    res.status(400).json({ error: "erro ao criar usuario" });
+    res.status(450).json({ messege: "erro ao criar usuario" });
+  }
+});
+
+//login
+router.post("/login", async (req, res) => {
+  try {
+    const userInfo = req.body;
+
+    //Busca o usuário no banco de dados
+    const user = await prisma.user.findUnique({
+      where: { email: userInfo.email },
+    });
+
+    //Verifica se o usuário existe dentro do banco
+    if (!user) {
+      return res.status(404).json({ messege: "Usuario não encontrado" });
+    }
+
+    //Compara a senha fornecida com a senha criptografada do banco
+    const isMatch = await bcrypt.compare(userInfo.password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ messege: "Senha incorreta" });
+    }
+
+    //Gera um token JWT
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "1m" });
+
+    res.status(200).json(token);
+  } catch (error) {
+    res.status(500).json({ messege: "Error ao fazer login" });
   }
 });
 
