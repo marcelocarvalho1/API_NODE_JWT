6d397e40603833b68b7759d08413ade73d15f184
Marcelo <marceloo.dev@gmail.com>
2025-03-12 14:44:48 -0300

    Server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import publicRoutes from "./routes/public.js";
+
+const app = express();
+app.use(express.json());
+
+app.use('/', publicRoutes);
+
+
+
+
+
+app.listen(3000, () => {
+  console.log("server rodando na porta 3000");
+});
+
+
